7f22a385b39dc4a92c2374454798f432e4c8b503
424246222 <47046347+424246222@users.noreply.github.com>
2019-01-26 10:57:31 +0800

    Update MyGit.java

--- Mygit/src/com/beans/MyGit.java
+++ Mygit/src/com/beans/MyGit.java
@@ -3,7 +3,7 @@ package com.beans;
 public class MyGit {
 		private   String userName;
 		private  String  password;
-                private   int  id；
-			 private   int  age； 
+                private   int  id;
+			 private   int  age;
 
 }
